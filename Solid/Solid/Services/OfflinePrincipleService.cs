d5ffae6d3531a6539c57cdcdabffcc253ea06bd0
Jacob Duijzer <jacob@duijzer.com>
2020-01-06 07:39:58 +0100

    Added markdown text

--- Solid/Solid/Services/OfflinePrincipleService.cs
+++ Solid/Solid/Services/OfflinePrincipleService.cs
@@ -18,9 +18,57 @@ namespace Solid.Services
                     Id = 1,
                     Name = "Single Responsibility Principle",
                     ShortDescription = "A class should have one, and only one, reason to change",
-                    Description = "# Single Responsibility Principle\n\nA class should have only one responsibility which means a class should be highly cohesive and implement strongly related logic. A class implementing feature 1 AND feature 2 AND feature 3 (and so on) violates SRP."
+                    Description = @"
+# Single Responsibility Principle
 
-        },
+A class should have only one responsibility which means a class should be highly cohesive and implement strongly related logic.
+
+
+A class implementing feature 1 AND feature 2 AND feature 3 (and so on) violates SRP.
+
+
+```
+public class Employee
+{
+    public int Employee_Id { get; set; }
+    public string Employee_Name { get; set; }
+
+    /// <summary>
+    /// This method used to insert into employee table
+    /// </summary>
+    /// <param name=""em"">Employee object</param>
+    /// <returns>Successfully inserted or not</returns>
+    public bool InsertIntoEmployeeTable(Employee em)
+    {
+        // Insert into employee table.
+        return true;
+    }
+    /// <summary>
+    /// Method to generate report
+    /// </summary>
+    /// <param name=""em""></param>
+    public void GenerateReport(Employee em)
+    {
+        // Report generation with employee data using crystal report.
+    }
+}
+```
+
+```
+public class ReportGeneration
+{
+    /// <summary>
+    /// Method to generate report
+    /// </summary>
+    /// <param name=""em""></param>
+    public void GenerateReport(Employee em)
+    {
+        // Report reneration with employee data.
+    }
+}
+```
+"
+                },
                 new Principle
                 {
                     Id = 2,
